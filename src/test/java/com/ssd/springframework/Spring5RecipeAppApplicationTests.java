e82e48318101694e6558cb4b2162f4cd401d8d5e
sudha-github22 <sudha.jan90@gmail.com>
2022-06-01 12:20:18 +0100

    Initial commit

---
+++ src/test/java/com/ssd/springframework/Spring5RecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssd.springframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5RecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
