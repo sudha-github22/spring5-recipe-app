a2965cb5fc45e6df20854bfc49935112c1071199
sudha-github22 <sudha.jan90@gmail.com>
2022-06-01 12:27:55 +0100

    Initial commit

--- src/main/java/com/ssd/springframework/Spring5RecipeAppApplication.java
+++ src/main/java/com/ssd/springframework/Spring5RecipeAppApplication.java
@@ -8,6 +8,7 @@ public class Spring5RecipeAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Spring5RecipeAppApplication.class, args);
+        System.out.println("Hi Everyone...");
     }
 
 }
