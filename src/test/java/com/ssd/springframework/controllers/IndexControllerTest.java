bbdf3772d6b38a234cebec0cf165135cbc53cf3e
sudha-github22 <sudha.jan90@gmail.com>
2022-06-14 21:24:41 +0100

    IndexController Test is written

---
+++ src/test/java/com/ssd/springframework/controllers/IndexControllerTest.java
@@ -0,0 +1,41 @@
+package com.ssd.springframework.controllers;
+
+import com.ssd.springframework.services.RecipeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anySet;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+public class IndexControllerTest {
+    @Mock
+    RecipeService recipeService;
+
+    @Mock
+    Model model;
+
+    IndexController indexController;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        indexController = new IndexController(recipeService);
+
+    }
+
+    @Test
+    public void getIndexPage() {
+        String viewName = indexController.getIndexPage(model);
+        assertEquals("index",viewName);
+
+       verify(recipeService,times(1)).getRecipes();
+       verify(model,times(1)).addAttribute(eq("recipes"),anySet());
+    }
+}
